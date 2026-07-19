916a26a971a28bd133527eb4dd9492cf1817eccd
Sergey Stukalov <suslo418@gmail.com>
2026-07-19 06:09:15 +0300

    Правки после ревью 2

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,26 +5,6 @@ from django_filters.widgets import BooleanWidget
 from .models import Recipe, Tag
 
 
-# class RecipeFilter(django_filters.FilterSet):
-
-#     tags = django_filters.ModelMultipleChoiceFilter(
-#         field_name='tags__slug',
-#         to_field_name='slug',
-#         queryset=Tag.objects.all(),
-#     )
-
-#     is_favorited = django_filters.BooleanFilter()
-#     is_in_shopping_cart = django_filters.BooleanFilter()
-
-#     class Meta:
-#         model = Recipe
-#         fields = (
-#             'tags',
-#             'author',
-#             'is_favorited',
-#             'is_in_shopping_cart',
-#         )
-
 class RecipeFilter(django_filters.FilterSet):
 
     tags = django_filters.ModelMultipleChoiceFilter(
@@ -43,7 +23,6 @@ class RecipeFilter(django_filters.FilterSet):
             'author',
             'is_favorited',
         )
-
 
 
 class IngredientsSearchFilter(SearchFilter):
